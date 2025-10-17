1b0b1a62edcc37c84bffa500ef4dc832ca36f09f
Susovan <susovan.sahoo777@gmail.com>
2025-10-17 12:06:18 +0530

    Added CRUD operations for Book Store

--- Book Store/backend/controller/bookController.js
+++ Book Store/backend/controller/bookController.js
@@ -1,27 +1,102 @@
-const getAllBooks = (req, res) => {
-  res.status(201).json({
-    status: "success",
-    // data: {}
-  });
+import { Book } from "../model/bookModel.js";
+
+const getAllBooks = async (req, res) => {
+  try {
+    const allBooks = await Book.find();
+
+    res.status(200).json({
+      status: "success",
+      result: allBooks.length,
+      data: {
+        allBooks,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      status: "Fail",
+      messege: err,
+    });
+  }
 };
 
-const getBook = (req, res) => {
-  res.status(201).json({
-    status: "success",
-    // data: {}
-  });
+const getBook = async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        book,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      status: "Fail",
+      messege: err,
+    });
+  }
 };
 
-const addNewBook = (req, res) => {
-  res.send("Request sent!");
+const addNewBook = async (req, res) => {
+  try {
+    const requestBody = req.body;
+    const newBook = await Book.create(requestBody);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        newBook,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      status: "Fail",
+      messege: err,
+    });
+  }
 };
 
-const updateBook = (req, res) => {
-  res.send("Request updated!");
+const updateBook = async (req, res) => {
+  try {
+    const book = await Book.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        book,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      status: "Fail",
+      messege: err,
+    });
+  }
 };
 
-const deleteBook = (req, res) => {
-  res.send("Book deleted!");
+const deleteBook = async (req, res) => {
+  try {
+    const book = await Book.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        book,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      status: "Fail",
+      messege: err,
+    });
+  }
 };
 
 export { getAllBooks, getBook, addNewBook, updateBook, deleteBook };
