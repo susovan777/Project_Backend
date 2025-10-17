08e4d1c859cf82765aa180e08930ac9f14dadddb
Susovan <susovan.sahoo777@gmail.com>
2025-10-17 16:51:12 +0530

    Startd a react app: Starting files added

---
+++ Book Store/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Routes>
+      {/* <Route path="" element={} />
+      <Route path="" element={} />
+      <Route path="" element={} />
+      <Route path="" element={} /> */}
+    </Routes>
+  )
+};
+
+export default App;
