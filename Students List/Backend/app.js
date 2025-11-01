098fc22325c18ed605dbe5b0ed63bf1a75f48cb2
Susovan <susovan.sahoo777@gmail.com>
2025-11-01 12:19:20 +0530

    New MERN project: Student List; Added files

--- Students List/Backend/app.js
+++ Students List/Backend/app.js
@@ -2,4 +2,13 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json())
+
+const htmlCode = "<h1>This is from server!</h1><h3>This is line</h3>"
+
+app.get("/", (req, res) => {
+    // res.status(200).send("Hello, from server!")
+    res.status(200).send(htmlCode)
+})
+
 export { app };
