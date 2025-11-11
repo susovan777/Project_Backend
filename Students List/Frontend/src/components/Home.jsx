7433001cc8799ab7e02e292ddc91f1aa09e88f9e
Susovan <susovan.sahoo777@gmail.com>
2025-11-11 09:36:43 +0530

    Added vercel.json file to deploy

--- Students List/Frontend/src/components/Home.jsx
+++ Students List/Frontend/src/components/Home.jsx
@@ -1,15 +1,12 @@
 // import { ToastContainer, toast } from "react-toastify";
-import toast, { Toaster } from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import Navbar from "./Navbar";
 import RecordList from "./RecordList";
 
 const Home = () => {
-  const notify = () => toast("Wow! so easy!");
-
   return (
     <div className="w-full p-6">
       <Navbar />
-      <button onClick={notify}>Notify!</button>
       <Toaster />
       <RecordList />
     </div>
