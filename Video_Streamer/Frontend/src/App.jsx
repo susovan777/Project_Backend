34596fd7b14eaf3a68ede27a3b999d077823ef58
Susovan <susovan.sahoo777@gmail.com>
2025-11-24 12:13:20 +0530

    Video player added and running successfully

--- Video_Streamer/Frontend/src/App.jsx
+++ Video_Streamer/Frontend/src/App.jsx
@@ -1,5 +1,41 @@
-const App = () => {
-  return <div>App</div>;
-};
+import VideoPlayer from "./VideoPlayer";
+import { useRef } from "react";
+
+function App() {
+  const playerRef = useRef(null);
+  const videoLink =
+    "http://localhost:8000/uploads/courses/-O6I12jcUK/index.m3u8";
+  const videoPlayerOptions = {
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: videoLink,
+        type: "application/x-mpegURL",
+      },
+    ],
+  };
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on("waiting", () => {
+      videojs.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      videojs.log("player will dispose");
+    });
+  };
+  return (
+    <>
+      <div className="main">
+        <h1>Video player</h1>
+        <VideoPlayer options={videoPlayerOptions} onReady={handlePlayerReady} />
+      </div>
+    </>
+  );
+}
 
 export default App;
